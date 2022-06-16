0f129390927c039f0980e05f9c8b2196d1d4bba0
Abdullah Ilgaz <mail@abdullahilgaz.com>
2022-06-17 01:53:17 +0300

    feat(postgres): resolves issues, bug fixes and improvements

---
+++ src/Utilities/FastQueryBuilder.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Fastql.Utilities
+{
+    public class FastQueryBuilder
+    {
+        private readonly List<QueryBuilderObject> _objects = new List<QueryBuilderObject>();
+
+        private readonly string _table;
+        private readonly string _where;
+
+        private string _identityColumn = "";
+
+        public FastQueryBuilder(string table, string where = null)
+        {
+            _table = table;
+            _where = where;
+        }
+
+        public void AddIdentityColumn(string identity)
+        {
+            _identityColumn = identity;
+        }
+
+        public void Add(string key, string name, object value)
+        {
+            var obj = new QueryBuilderObject(key, name, value);
+            if (_objects.Contains(obj))
+                throw new DuplicateNameException("This field was already declared");
+
+            _objects.Add(obj);
+        }
+
+        public void AddCondition(string parameterName, string name, object value)
+        {
+            var obj = new QueryBuilderObject(parameterName, name, value);
+            
+            if (_objects.Contains(obj))
+                throw new DuplicateNameException("This field was already declared");
+        }
+
+        public string InsertSql
+        {
+            get
+            {
+                if (_objects.Count == 0)
+                    throw new Exception("Input parameters not provided.");
+
+                var fields = string.Join(", ", _objects.Select(x => x.Key));
+                var values = string.Join(", @", _objects.Select(x => x.Name));
+                return $"INSERT INTO {_table}({fields}) VALUES(@{values});";
+            }
+        }
+
+        public string UpdateSql
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_where))
+                    throw new Exception("Where clause not provided.");
+
+                if (_objects.Count == 0)
+                    throw new Exception("Input parameters not provided.");
+
+                var sb = new StringBuilder();
+                foreach (var obj in _objects)
+                {
+                    sb.Append($"{obj.Key} = @{obj.Name}, ");
+                }
+
+                return $"UPDATE {_table} SET {sb.ToString().Substring(0, sb.Length - 2)} {_where};";
+            }
+        }
+
+        public string TableName
+        {
+            get
+            {
+                return _table;
+            }
+        }
+
+    }
+}
---
+++ src/Utilities/QueryBuilderObject.cs
@@ -0,0 +1,21 @@
+namespace Fastql.Utilities
+{
+    public class QueryBuilderObject
+    {
+        public string Key { get; set; }
+        public string Name { get; set; }
+        public object Value { get; set; }
+
+        public QueryBuilderObject()
+        {
+            
+        }
+        
+        public QueryBuilderObject(string key, string name, object value)
+        {
+            Key = key;
+            Name = name;
+            Value = value;
+        }
+    }
+}
